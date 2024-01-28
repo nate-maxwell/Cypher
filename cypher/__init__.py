34ff69eda6f0eb40c068948368dd9f1634ac0f47
nate-maxwell <natedmaxwell@gmail.com>
2024-01-27 19:52:01 -0800

    switching to css file

--- cypher/__init__.py
+++ cypher/__init__.py
@@ -6,9 +6,9 @@ from pathlib import Path
 from PySide2 import QtWidgets
 
 
-def set_qss(tool: QtWidgets.QWidget, qss_file: str = 'Combinear.qss'):
+def set_qss(tool: QtWidgets.QWidget, qss_file: str = 'darkorange.css'):
     qss_input_name = Path(qss_file)
-    qss_file_name = f'{qss_input_name.stem}.qss'
+    qss_file_name = f'{qss_input_name.stem}.css'
     qss_path = Path(Path(__file__).parent, 'resources', qss_file_name)
 
     with open(qss_path, 'r') as f:
